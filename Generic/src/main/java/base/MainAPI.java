88c6ada7b20a20dde415cb7819cdb2d5e6b27d11
Muhtasim Mahir <muhtasimmahir@Muhtasims-MacBook-Pro.local>
2019-03-17 17:36:51 -0400

    added generic module

---
+++ Generic/src/main/java/base/MainAPI.java
@@ -0,0 +1,26 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class MainAPI {
+    public WebDriver driver = null;
+    @BeforeMethod
+    public void setUp(String url) {
+        System.setProperty("webdriver.chrome.driver", "/Users/muhtasimmahir/Group4ProjectWebAutomation/Generic/driver/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.navigate().to(url);
+        driver.manage().window().maximize();
+        }
+        @AfterMethod
+        public void cleanUp(){
+            driver.close();
+        }
+
+}
