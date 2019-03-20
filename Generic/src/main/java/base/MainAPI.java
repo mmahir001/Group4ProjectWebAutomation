4f0b970bce505dcd54396797e7d27ca347bc5da3
Muhtasim Mahir <muhtasimmahir@Muhtasims-MacBook-Pro.local>
2019-03-20 16:19:53 -0400

    addded to my branch

--- Generic/src/main/java/base/MainAPI.java
+++ Generic/src/main/java/base/MainAPI.java
@@ -4,12 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 
 import java.util.concurrent.TimeUnit;
 
 public class MainAPI {
     public WebDriver driver = null;
+    @Parameters({"url"})
     @BeforeMethod
     public void setUp(String url) {
         System.setProperty("webdriver.chrome.driver", "/Users/muhtasimmahir/Group4ProjectWebAutomation/Generic/driver/chromedriver");
