f50ed15a283d5b31d516a47563ff121231131070
Nancy3850 <nancymanzar@gmail.com>
2019-03-30 02:27:45 -0400

    commit

--- TargetRetail/src/main/java/base/Home.java
+++ TargetRetail/src/main/java/base/Home.java
@@ -2,6 +2,7 @@ package base;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import reporting.TestLogger;
 
 public class Home extends MainAPI {
 
@@ -9,15 +10,17 @@ public class Home extends MainAPI {
     public static WebElement searchBox;
 
     public String clickSearchBox(){
-      searchBox.click();
-      String text = driver.getTitle();
-      return text;
+        TestLogger.log(getClass().getSimpleName() + ": " + MainAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        searchBox.click();
+        String text = driver.getTitle();
+        return text;
     }
 
     @FindBy(xpath = "//a[@id='cart']")
     WebElement cart;
 
     public String clickCart() {
+        TestLogger.log(getClass().getSimpleName() + ": " + MainAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         cart.click();
         String text = cart.getText();
         return text;
@@ -26,6 +29,7 @@ public class Home extends MainAPI {
     @FindBy(xpath = "//nav[@id='headerMain']//a[@id='account']")
     WebElement signIn;
     public String clickSignIn(){
+        TestLogger.log(getClass().getSimpleName() + ": " + MainAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         signIn.click();
         String text = signIn.getText();
         return text;
@@ -34,6 +38,7 @@ public class Home extends MainAPI {
     @FindBy(xpath = "//a[@id='trending']")
     WebElement treading;
     public String clickTreading(){
+        TestLogger.log(getClass().getSimpleName() + ": " + MainAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         treading.click();
         String text = treading.getText();
         return text;
@@ -42,6 +47,7 @@ public class Home extends MainAPI {
     @FindBy (xpath = "//a[@id='secondary']")
     WebElement deals;
     public String clickDeals(){
+        TestLogger.log(getClass().getSimpleName() + ": " + MainAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         deals.click();
         String text = deals.getText();
         return text;
@@ -50,6 +56,7 @@ public class Home extends MainAPI {
     @FindBy(xpath = "//a[@class='Link-sc-1khjl8b-0 lifmEv'][contains(text(),'Find Stores')]")
     WebElement findStore;
     public String clickFindStore (){
+        TestLogger.log(getClass().getSimpleName() + ": " + MainAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         findStore.click();
         String text =findStore.getText();
         return text;
@@ -58,6 +65,7 @@ public class Home extends MainAPI {
     @FindBy(xpath = "//a[@class='Link-sc-1khjl8b-0 lifmEv'][contains(text(),'Gift Cards')]")
     WebElement targetGiftCard;
     public String clickTargetGiftCard(){
+        TestLogger.log(getClass().getSimpleName() + ": " + MainAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         targetGiftCard.click();
         String text = targetGiftCard.getText();
         return text;
@@ -66,6 +74,7 @@ public class Home extends MainAPI {
     @FindBy(xpath = "//a[@class='Link-sc-1khjl8b-0 lifmEv'][contains(text(),'Weekly Ad')]")
     WebElement weeklyAd;
     public String clickWeeklyAd(){
+        TestLogger.log(getClass().getSimpleName() + ": " + MainAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         weeklyAd.click();
         String text = weeklyAd.getText();
         return text;
@@ -74,6 +83,7 @@ public class Home extends MainAPI {
     @FindBy(xpath = "//a[@class='Link-sc-1khjl8b-0 lifmEv'][contains(text(),'Registries & Lists')]")
     WebElement registriesLists;
     public String clickRegistriesList(){
+        TestLogger.log(getClass().getSimpleName() + ": " + MainAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         registriesLists.click();
         String text = registriesLists.getText();
         return text;
@@ -82,6 +92,7 @@ public class Home extends MainAPI {
     @FindBy(xpath = "//a[@class='Link-sc-1khjl8b-0 lifmEv'][contains(text(),'Orders')]")
     WebElement order;
     public String clickOrder(){
+        TestLogger.log(getClass().getSimpleName() + ": " + MainAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         order.click();
         String text = order.getText();
         return text;
